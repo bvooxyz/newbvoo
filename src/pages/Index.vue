1a93e9d7d9d8f80c7b55e19db001d527073fdd7e
bvoo <bvoowo@gmail.com>
2021-05-22 00:57:36 -0700

    css anim

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,17 @@
 <template>
   <q-page class="flex flex-center">
     <div class="fit row wrap justify-start">
-      <div id="mainheader" class="text-h1">bvoo.xyz</div>
+      <div id="mainheader" class="text-h1 typing-main">bvoo.xyz</div>
       <div class="itemlist column wrap">
-        <div class="text-h4">
+        <div class="text-h4 typing-list">
           <q-item clickable @click="$router.replace('/Projects')"
             >Projects</q-item
           >
         </div>
-        <div class="text-h4">
+        <div class="text-h4 typing-list">
           <q-item clickable @click="$router.replace('/ROMs')">ROMs</q-item>
         </div>
-        <div class="text-h4">
+        <div class="text-h4 typing-list">
           <q-item clickable @click="$router.replace('/awa')">awa</q-item>
         </div>
       </div>
