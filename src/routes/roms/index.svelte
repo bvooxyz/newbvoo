00db98e25d886acdb8edd4a3f5da7009774d639d
bvoo <60059541+bvoo@users.noreply.github.com>
2022-01-17 09:46:22 -0800

    oops

--- src/routes/roms/index.svelte
+++ src/routes/roms/index.svelte
@@ -5,8 +5,8 @@
     <div class="h-2" />
     <div class='pt-4 text-4xl flex flex-col gap-5'>
       <span class='font-mono text-secondary'><a href='https://newchallenger.net/fc2/fc2roms.zip'>FC2 JSONs</a></span>
-      <span class='font-mono text-secondary'><a href="https://cdn.krypton.sh/cps3/jojoba.zip">jojoba</a></span>
-      <span class='font-mono text-secondary'><a href="https://cdn.krypton.sh/cps3/jojobanr1.zip">jojobanr1</a></span>
+      <span class='font-mono text-secondary'><a href="https://cdn.krypton.sh/jojoba.zip">jojoba</a></span>
+      <span class='font-mono text-secondary'><a href="https://cdn.krypton.sh/jojobanr1.zip">jojobanr1</a></span>
       <div class='h-[2em]' />
       <span class='font-mono text-secondary text-2xl'><a href="https://krypton.sh/">Coming Soon</a></span>
     </div>
