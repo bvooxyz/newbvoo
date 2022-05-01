0645b9ec22e788391e555e48675b04485664b16d
bvoo <60059541+bvoo@users.noreply.github.com>
2022-04-30 17:13:08 -0700

    embed

--- src/routes/tools/index.svelte
+++ src/routes/tools/index.svelte
@@ -4,9 +4,9 @@
     <h1 class='font-serif text-8xl font-bold tracking-wider pb-16'>Tools</h1>
     <div class="h-2" />
     <div class='pt-4 text-4xl flex flex-col gap-5'>
-      <span class='font-mono text-secondary'><a href='https://b64.bvoo.xyz'>Base64 Encoder / Decoder</a></span>
+      <span class='font-mono text-secondary'><a href='https://b64.bvoo.xyz/'>Base64 Encoder / Decoder</a></span>
+      <span class='font-mono text-secondary'><a href="https://embed.bvoo.xyz/">Embed Generator</a></span>
       <span class='font-mono text-secondary'><a href="https://krypton.sh/">krypton.sh</a></span>
-      <span class='font-mono text-secondary'><a href="/soon">Soon</a></span>
     </div>
   </div>
 </div>
