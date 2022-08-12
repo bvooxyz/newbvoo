30735429dfb5d96101d06e707cf76cfeef26d046
bvoo <60059541+bvoo@users.noreply.github.com>
2022-08-12 08:55:04 -0700

    Update index.svelte

--- src/routes/roms/index.svelte
+++ src/routes/roms/index.svelte
@@ -4,7 +4,7 @@
     <h1 class='font-serif text-8xl font-bold tracking-wider pb-16'>ROMs</h1>
     <div class="h-2" />
     <div class='pt-4 text-4xl flex flex-col gap-5'>
-      <span class='font-mono text-secondary'><a href='https://fightcade.download/fc2json.zip'>FC2 JSONs</a></span>
+      <span class='font-mono text-secondary'><a href='https://fightcade.guide/'>FC2 JSONs</a></span>
       <span class='font-mono text-secondary'><a href="https://krypton.sh/">JoJo ROMs have moved!</a></span>
       <span class='font-mono text-secondary'><a href="https://krypton.sh/">You can find them on krypton.sh</a></span>
     </div>
